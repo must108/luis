3a6f4c85692b4a477583876674fa4e1c88d559fc
must108 <mustaeen18@gmail.com>
2024-12-15 13:40:00 -0500

    add robots, readme, page

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 
 import React from 'react';
-import { cn } from '@/utils/cn';
 
 
 export default function Home() {  return (
-    <div>
-      luis hidalgo
-    </div>
+    <main>
+      <header className=''>
+        Luis Hidalgo
+      </header>
+    </main>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { cn } from "@/utils/cn";
 
 const inter = Inter({ subsets: ["latin"] });
 
